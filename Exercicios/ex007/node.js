bce6677af764ec30968fbddecfe9668470f2e67f
Alex Simão de Souza <126366771+AlexSimao@users.noreply.github.com>
2023-06-14 20:18:22 -0300

    Laços de repetições.

---
+++ Exercicios/ex007/node.js
@@ -0,0 +1,15 @@
+
+fatias = 8
+
+// Estrutura de repetição de teste lógico no início
+while (fatias >= 1) {
+    console.log(fatias)
+    fatias--
+}
+
+
+// Estrutura de repetição de teste lógico no final
+/* do {
+    console.log(fatias)
+    fatias--
+} while (fatias >= 1) */
