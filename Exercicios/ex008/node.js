ec04bae4c0c9ecd3bb0f8cc637d78515a9251a7b
Alex Simão de Souza <126366771+AlexSimao@users.noreply.github.com>
2023-06-22 22:25:34 -0300

    Aprendendo For-In Loop.

--- Exercicios/ex008/node.js
+++ Exercicios/ex008/node.js
@@ -10,3 +10,17 @@ console.log(num) // Imprima num
 console.log(num.length) // Imprima o comprimento/"quantidade de elemetos"
 console.log(num.sort()) // Imprima o num organizado
 
+/* for (let pos=0; pos < num.length; pos++){
+    console.log(`Índice ${pos} = ${num[pos]}`)
+} // Imprimindo todos os elementos do array */
+
+for (let pos in num){
+    console.log(`Índice ${pos} = ${num[pos]}`)
+} // (simplificado com for-In)
+
+
+// ===============================================
+
+var id = ["Alex", "Simão", new Date().getFullYear() - 2003]
+
+console.log(`Meu nome é ${id[0]} ${id[1]} e tenho ${id[2]} anos.`)
