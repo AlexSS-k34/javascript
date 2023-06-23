e736dceabbde9a1b6cec52184d9f61a126de5372
Alex Simão de Souza <126366771+AlexSimao@users.noreply.github.com>
2023-06-22 22:11:00 -0300

    Aprendendo sobre arrays.

---
+++ Exercicios/ex008/node.js
@@ -0,0 +1,12 @@
+// Variaveis Compostas/Vetores/Arrays
+
+var num = [5,2,8] 
+console.log(num) // Imprima num
+console.log(num[0]) // Imprima o item do índice 0
+console.log(num[1]) // Imprima o item do índice 1
+console.log(num[2]) // Imprima o item do índice 2
+num.push(4) // Adicione novo elemento com o valor 4
+console.log(num) // Imprima num
+console.log(num.length) // Imprima o comprimento/"quantidade de elemetos"
+console.log(num.sort()) // Imprima o num organizado
+
