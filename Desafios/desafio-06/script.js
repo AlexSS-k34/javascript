1008700ef24289d8dd8e171196f18819d32d0ae1
Alex Simão de Souza <126366771+AlexSimao@users.noreply.github.com>
2023-06-26 00:03:14 -0300

    Interrupção de números repetidos.
    adicionando condições no código para não permitir números repetidos na lista.

--- Desafios/desafio-06/script.js
+++ Desafios/desafio-06/script.js
@@ -4,17 +4,19 @@ var sel = document.querySelector('#sel')
 var tab = []
 
 function adicionar(){
-    if (txtnum.value.length = 0 || txtnum.value < 1 || txtnum.value > 100){
-        alert('[ERRO] Valor inserido invalido.')
+    if (txtnum.value.length = 0 || txtnum.value < 1 || txtnum.value > 100 || tab.indexOf(Number(txtnum.value)) != -1){
+        alert('[ERRO] Valor invalido ou ja adicionado.')
+
     } else{
-        let num = Number(txtnum.value)
+        let num = Number(txtnum.value)        
         let new_opt = document.createElement("option")
         new_opt.text = `Valor ${num} adicionado`
         sel.appendChild(new_opt)
         tab.push(num)
         txtnum.value = ""
-        console.log(tab)
     }
+    console.log(tab)
+    console.log(tab.indexOf(Number(txtnum.value)))
 }
 
 
