652e9d49c9c405c909ae8c3a7bdbe59cba503390
Alex Simão de Souza <126366771+AlexSimao@users.noreply.github.com>
2023-06-25 22:40:16 -0300

    Guardando os valores.
    Criando os options e mostrando na tela os números adicionados. E criando um array para guardar todos os valores.

---
+++ Desafios/desafio-06/script.js
@@ -0,0 +1,20 @@
+
+var txtnum = document.querySelector('#txtnum')
+var sel = document.querySelector('#sel')
+var tab = []
+
+function adicionar(){
+    if (txtnum.value.length = 0 || txtnum.value < 1 || txtnum.value > 100){
+        alert('[ERRO] Valor inserido invalido.')
+    } else{
+        let num = Number(txtnum.value)
+        let new_opt = document.createElement("option")
+        new_opt.text = `Valor ${num} adicionado`
+        sel.appendChild(new_opt)
+        tab.push(num)
+        txtnum.value = ""
+        console.log(tab)
+    }
+}
+
+
