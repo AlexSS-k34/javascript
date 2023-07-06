f41ff392f4366d43d1dfb98feacba6c8c78560a9
Alex Simão de Souza <126366771+AlexSimao@users.noreply.github.com>
2023-07-06 00:56:17 -0300

    Aprendendo sobre Objetos, Propriedades e Métodos.

---
+++ Exercicios/ex011/node.js
@@ -0,0 +1,21 @@
+
+// Objeto
+
+var pessoa = { // Objeto pessoa.
+    nome: "Alex",
+    sobreNome: ["Simão", "Souza"],
+    idade: 20,
+    altura: 1.66,
+    casado: false,
+    estudando: true,
+    aniversario: () => { // Cada vez que essa função/método for invocado, a idade aumenta em +1. 
+        pessoa.idade++
+    },
+    nomeSobreNome: () => { // Quando esse método é invovada, o pessoa.nome se torna o nome completo.
+        pessoa.nome = `${pessoa.nome} ${pessoa.sobreNome[0]} de ${pessoa.sobreNome[1]}`
+    }
+}
+pessoa.aniversario()
+pessoa.nomeSobreNome()
+
+console.log(pessoa)
