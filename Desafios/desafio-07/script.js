dd3758b44869ceddf0df3c000a1cace5ecb741df
Alex Simão de Souza <126366771+AlexSimao@users.noreply.github.com>
2023-07-12 00:10:40 -0300

    Terminando a lógica.

--- Desafios/desafio-07/script.js
+++ Desafios/desafio-07/script.js
@@ -1,11 +1,17 @@
-// const docRelogio = document.querySelector("#relogio")
-const date = new Date
-const relogio = {
-    horas: date.getHours(),
-    minutos: date.getMinutes(),
-    segundos: date.getSeconds(),
-}
+setInterval(() => {
+    const docHora = document.querySelector("#hora")
+    const docMin = document.querySelector("#min")
+    const docSec = document.querySelector("#sec")
 
-console.log(relogio.horas, relogio.minutos, relogio.segundos)
+    const date = new Date
+    const relogio = {
+        horas: date.getHours(),
+        minutos: date.getMinutes(),
+        segundos: date.getSeconds(),
+    }
 
+    docHora.textContent = `${relogio.horas}`
+    docMin.textContent = `${relogio.minutos}`
+    docSec.textContent = `${relogio.segundos}`
+}, 1000)
 
