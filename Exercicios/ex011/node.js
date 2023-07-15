133c4da3e6873d6064ecf5d066683293a439a72e
Alex Simão de Souza <126366771+AlexSimao@users.noreply.github.com>
2023-07-14 23:46:55 -0300

    IIFE.

--- Exercicios/ex011/node.js
+++ Exercicios/ex011/node.js
@@ -1,21 +1,23 @@
 
-// Objeto
-
-var pessoa = { // Objeto pessoa.
-    nome: "Alex",
-    sobreNome: ["Simão", "Souza"],
-    idade: 20,
-    altura: 1.66,
-    casado: false,
-    estudando: true,
-    aniversario: () => { // Cada vez que essa função/método for invocado, a idade aumenta em +1. 
-        pessoa.idade++
-    },
-    nomeSobreNome: () => { // Quando esse método é invovada, o pessoa.nome se torna o nome completo.
-        pessoa.nome = `${pessoa.nome} ${pessoa.sobreNome[0]} de ${pessoa.sobreNome[1]}`
+(() => { // IIFE == Immediately Invoked Function Expression.
+    
+    // Objeto
+    var pessoa = { // Objeto pessoa.
+        nome: "Alex",
+        sobreNome: ["Simão", "Souza"],
+        idade: 20,
+        altura: 1.66,
+        casado: false,
+        estudando: true,
+        aniversario: () => { // Cada vez que essa função/método for invocado, a idade aumenta em +1. 
+            pessoa.idade++
+        },
+        nomeSobreNome: () => { // Quando esse método é invovada, o pessoa.nome se torna o nome completo.
+            pessoa.nome = `${pessoa.nome} ${pessoa.sobreNome[0]} de ${pessoa.sobreNome[1]}`
+        }
     }
-}
-pessoa.aniversario()
-pessoa.nomeSobreNome()
+    pessoa.aniversario()
+    pessoa.nomeSobreNome()
 
-console.log(pessoa)
+    console.log(pessoa)
+})()
