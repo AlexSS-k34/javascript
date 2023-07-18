eec4fc849938b0ae14423e4f769ecf8ff35019bf
Alex Simão de Souza <126366771+AlexSimao@users.noreply.github.com>
2023-07-18 19:20:48 -0300

    Prova de conhecimento...

---
+++ Exercicios/ex013/script.js
@@ -0,0 +1,14 @@
+document.write(" <h2>Informações </h2>")
+
+let nomeLutador = "Anderson Silva" 
+let nacLutador = "Brasilero"
+let idadeLutador = 48
+let pesoLutador = 84.4
+let alturaLutador = 1.88
+
+document.write(`<p> Lutador: ${nomeLutador} </p>`)
+document.write(`<p> Nacionalidade: ${nacLutador} </p>`)
+document.write("<p> Idade: " + idadeLutador + " anos </p>")
+document.write("<p> Peso: " + pesoLutador + " Kg </p>")
+document.write("<p> Altura: " + alturaLutador + " m </p> <hr>")
+//=====================================================
