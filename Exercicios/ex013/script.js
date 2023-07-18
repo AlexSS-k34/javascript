5289869064e2ee484b50e9ffb90a8b0dad6a8146
Alex Simão de Souza <126366771+AlexSimao@users.noreply.github.com>
2023-07-18 19:21:23 -0300

    Update script.js

--- Exercicios/ex013/script.js
+++ Exercicios/ex013/script.js
@@ -24,3 +24,10 @@ const autor = "Desconhecido"
 document.write(`<p> ${dia}/${mes}/${ano} </p>`)
 document.write(`<p> ${frase} </p> <hr>`)
 //=====================================================
+
+document.write("<h2> Array de Meses do Ano </h2>")
+
+const meses = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"]
+
+document.write(`<p> ${meses[0]}, ${meses[1]}, ${meses[2]}, ${meses[3]}, ${meses[4]}, ${meses[5]}, ${meses[6]}, ${meses[7]}, ${meses[8]}, ${meses[9]}, ${meses[10]}, ${meses[11]} </p> <hr>`)
+//======================================================
