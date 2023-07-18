8e2cf0123396ad21547a2cf13b793b1384045a84
Alex Simão de Souza <126366771+AlexSimao@users.noreply.github.com>
2023-07-18 19:21:12 -0300

    Update script.js

--- Exercicios/ex013/script.js
+++ Exercicios/ex013/script.js
@@ -12,3 +12,15 @@ document.write("<p> Idade: " + idadeLutador + " anos </p>")
 document.write("<p> Peso: " + pesoLutador + " Kg </p>")
 document.write("<p> Altura: " + alturaLutador + " m </p> <hr>")
 //=====================================================
+
+document.write("<h2> Frase do Dia </h2>")
+
+const dia = 10
+const mes = 9
+const ano = 2021
+const frase = "Toda manhã você tem duas escolhas: continuar dormindo com seus sonhos ou acordar e persegui-los!"
+const autor = "Desconhecido"
+
+document.write(`<p> ${dia}/${mes}/${ano} </p>`)
+document.write(`<p> ${frase} </p> <hr>`)
+//=====================================================
