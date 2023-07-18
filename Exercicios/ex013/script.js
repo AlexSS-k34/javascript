49f38a29d36ccc56c9996f3713e987bdc2510705
Alex Simão de Souza <126366771+AlexSimao@users.noreply.github.com>
2023-07-18 19:29:01 -0300

    Update script.js

--- Exercicios/ex013/script.js
+++ Exercicios/ex013/script.js
@@ -8,9 +8,9 @@ let alturaLutador = 1.88
 
 document.write(`<p> Lutador: ${nomeLutador} </p>`)
 document.write(`<p> Nacionalidade: ${nacLutador} </p>`)
-document.write("<p> Idade: " + idadeLutador + " anos </p>")
-document.write("<p> Peso: " + pesoLutador + " Kg </p>")
-document.write("<p> Altura: " + alturaLutador + " m </p> <hr>")
+document.write(`<p> Idade: ${idadeLutador} anos </p>`)
+document.write(`<p> Peso: ${pesoLutador} Kg </p>`)
+document.write(`<p> Altura: ${alturaLutador} m </p> <hr>`)
 //=====================================================
 
 document.write("<h2> Frase do Dia </h2>")
