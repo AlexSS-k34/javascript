7530f8ab6b36589b428817e05b7614a7330240ce
Alex Simão de Souza <126366771+AlexSimao@users.noreply.github.com>
2023-07-18 19:44:42 -0300

    Organizando...

--- Exercicios/ex013/script.js
+++ Exercicios/ex013/script.js
@@ -6,11 +6,13 @@ let idadeLutador = 48
 let pesoLutador = 84.4
 let alturaLutador = 1.88
 
-document.write(`<p> Lutador: ${nomeLutador} </p>`)
-document.write(`<p> Nacionalidade: ${nacLutador} </p>`)
-document.write(`<p> Idade: ${idadeLutador} anos </p>`)
-document.write(`<p> Peso: ${pesoLutador} Kg </p>`)
-document.write(`<p> Altura: ${alturaLutador} m </p> <hr>`)
+document.write(`
+    <p> Lutador: ${nomeLutador} </p>
+    <p> Nacionalidade: ${nacLutador} </p>
+    <p> Idade: ${idadeLutador} anos </p>
+    <p> Peso: ${pesoLutador} Kg </p>
+    <p> Altura: ${alturaLutador} m </p>
+`)
 //=====================================================
 
 document.write("<h2> Frase do Dia </h2>")
@@ -21,15 +23,19 @@ const ano = 2021
 const frase = "Toda manhã você tem duas escolhas: continuar dormindo com seus sonhos ou acordar e persegui-los!"
 const autor = "Desconhecido"
 
-document.write(`<p> ${dia}/${mes}/${ano} </p>`)
-document.write(`<p> ${frase} </p> <hr>`)
+document.write(`
+    <p> ${dia}/${mes}/${ano} </p>
+    <p> ${frase} </p>
+`)
 //=====================================================
 
 document.write("<h2> Array de Meses do Ano </h2>")
 
 const meses = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"]
 
-document.write(`<p> ${meses[0]}, ${meses[1]}, ${meses[2]}, ${meses[3]}, ${meses[4]}, ${meses[5]}, ${meses[6]}, ${meses[7]}, ${meses[8]}, ${meses[9]}, ${meses[10]}, ${meses[11]} </p> <hr>`)
+document.write(`
+    <p> ${meses[0]}, ${meses[1]}, ${meses[2]}, ${meses[3]}, ${meses[4]}, ${meses[5]}, ${meses[6]}, ${meses[7]}, ${meses[8]}, ${meses[9]}, ${meses[10]}, ${meses[11]} </p>
+`)
 //======================================================
 
 document.write("<h2> Objeto jogo </h2>")
@@ -40,6 +46,8 @@ const jogoObj = {
     anoLancamento: 2020
 }
 
-document.write(`<p> Jogo: ${jogoObj.jogo} </p>`)
-document.write(`<p> Desenvolvido por: ${jogoObj.desenvolvedora} </p>`)
-document.write(`<p> Ano de Lançamento: ${jogoObj.anoLancamento} </p>`)
+document.write(`
+    <p> Jogo: ${jogoObj.jogo} </p>
+    <p> Desenvolvido por: ${jogoObj.desenvolvedora} </p>
+    <p> Ano de Lançamento: ${jogoObj.anoLancamento} </p>
+`)
